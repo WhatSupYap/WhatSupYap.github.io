80414ae8cb69e952e6520d7175fe3a987735f36f
= <=>
2024-03-28 08:54:14 +0900

    2024.03.28.1

--- assets/js/wsy/xCommon.js
+++ assets/js/wsy/xCommon.js
@@ -1,5 +1,4 @@
-function xCalPer(start,end,ing)
-{
+function xCalPer(start, end, ing) {
   //var 문제개수 = end - start + 1;
 
   var x = end - start + 1;
@@ -8,12 +7,22 @@ function xCalPer(start,end,ing)
   var per = "";
   var star = "";
 
-  if (100 == z){ per = "10000"; star = "★★★"}
-  else if (90 < z){ per = "9199"; star = "★★☆" }
-  else if (80 < z){ per = "8190"; star = "★★☆" }
-  else if (70 < z){ per = "7180"; star = "★☆☆" }
-  else if (60 < z){ per = "6170"; star = "★☆☆" }
-  else {per = "0060"; star = "☆☆☆"}
+  if (100 == z) { per = "10000"; star = "★★★" }
+  else if (90 < z) { per = "9199"; star = "★★☆" }
+  else if (80 < z) { per = "8190"; star = "★★☆" }
+  else if (70 < z) { per = "7180"; star = "★☆☆" }
+  else if (60 < z) { per = "6170"; star = "★☆☆" }
+  else { per = "0060"; star = "☆☆☆" }
   var rst = "<span class=\"per" + per + "\">" + star + "</span>";
   document.write(rst);
+}
+
+function xIsEmpty(p) {
+  if (typeof p === "undefined") {
+    return true;
+  }
+  else if (p.toString() === "") {
+    return true;
+  }
+  return false;
 }
