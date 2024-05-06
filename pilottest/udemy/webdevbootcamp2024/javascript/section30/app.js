849625f033b58d9dba70763632231d17c7ce6ad3
What5up7 <bumsup.kim@gmail.com>
2024-05-07 08:07:35 +0900

    개인PC.20240507.1

--- pilottest/udemy/webdevbootcamp2024/javascript/section30/app.js
+++ pilottest/udemy/webdevbootcamp2024/javascript/section30/app.js
@@ -1,1 +1,6 @@
 ﻿
+for (let i = 0; i < 10; i++)
+{
+    console.log(`hellow from first script${i+1}`);    
+}
+console.log(process.argv);
---
+++ pilottest/udemy/webdevbootcamp2024/javascript/section30/boilerplate.js
@@ -0,0 +1,15 @@
+﻿const fs = require('fs');
+const folderName = process.argv[2] || 'Project'
+//console.log(fs);
+
+// fs.mkdir('Dogs', {recursive: true}, (err) => {
+//     console.log("IN THE CALLBACK!!")
+//     if(err) throw err;
+// });
+
+// fs.mkdirSync("Cats");
+// console.log("I COME AFTER MKDIR IN THE FILE!!");
+fs.mkdirSync(folderName);
+fs.writeFileSync(`${folderName}/index.html`, "<html></html>");
+fs.writeFileSync(`${folderName}/app.js`, `console.log("abc")`);
+fs.writeFileSync(`${folderName}/style.css`, "123");
