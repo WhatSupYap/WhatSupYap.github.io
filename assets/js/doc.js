2dc176d532896498ee58771a2fb71c5dad28f05b
What5up7 <bumsup.kim@gmail.com>
2024-05-10 08:50:09 +0900

    20240510.개인pc

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -1,14 +1,4 @@
-﻿String.prototype.right = function (length) {
-
-    if (this.length <= length) {
-        return this;
-    }
-    else {
-        return this.substring(this.length - length, this.length);
-    }
-}
-
-const now = new Date();
+﻿const now = new Date();
 const year = now.getFullYear();
 const month = ("00" + (now.getMonth() + 1)).right(2); // 월은 0부터 시작하기 때문에 1을 더합니다.
 const day = ("00" + now.getDate()).right(2);
@@ -18,4 +8,4 @@ const second = ("00" + now.getSeconds()).right(2);
 
 const spanToday = document.querySelector("#today");
 
-spanToday.innerHTML = `${year}.${month}.${day}`;
+if(spanToday !== undefined) spanToday.innerHTML = `${year}.${month}.${day}`;
---
+++ pilottest/udemy/webdevbootcamp2024/node/s33_Express/index.js
@@ -0,0 +1,20 @@
+﻿const express = require("express");
+const app = express();
+//console.dir(app);
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World')
+// });
+
+app.use((req, res) => {
+    console.log("WE GOT A NEW REQUEST!!");
+    console.dir(req);
+    res.send("Hello!");
+});
+
+
+const port = 8080;
+
+app.listen(port, () => {
+    console.log(`LISTENING OF PORT ${port}`);
+});
