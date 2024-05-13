995f495bba92b17bf7beaa36882ba74d7aab38b3
What5up7 <bumsup.kim@gmail.com>
2024-05-14 08:46:09 +0900

    20240514.개인pc

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -1,4 +1,7 @@
-﻿const now = new Date();
+﻿String.prototype.left = function (length) { if (this.length <= length) return this; else return this.substring(0, length); }
+String.prototype.right = function (length) { if (this.length <= length) return this; else return this.substring(this.length - length, this.length); }
+
+const now = new Date();
 const year = now.getFullYear();
 const month = ("00" + (now.getMonth() + 1)).right(2); // 월은 0부터 시작하기 때문에 1을 더합니다.
 const day = ("00" + now.getDate()).right(2);
@@ -7,5 +10,22 @@ const minute = ("00" + now.getMinutes()).right(2);
 const second = ("00" + now.getSeconds()).right(2);
 
 const spanToday = document.querySelector("#today");
+if (spanToday !== undefined && spanToday !== null) spanToday.innerHTML = `${year}.${month}.${day}`;
 
-if(spanToday !== undefined) spanToday.innerHTML = `${year}.${month}.${day}`;
+const blockquoteList = document.querySelectorAll("blockquote");
+
+for (const blockquote of blockquoteList) {
+    const splitedList = blockquote.innerHTML.split("\n");
+    let blockquoteInnerHTML = "";
+    for (const line of splitedList) {
+        let cleanedline = line.trim();
+        if (cleanedline.length === 0) continue;
+        let thisLine = "";
+        if (blockquoteInnerHTML !== "") thisLine = "<br>";
+        thisLine += line;
+        blockquoteInnerHTML += thisLine;
+    }
+    blockquote.innerHTML = blockquoteInnerHTML;
+}
+
+
--- pilottest/udemy/webdevbootcamp2024/node/s34_dynamicHTML/index.js
+++ pilottest/udemy/webdevbootcamp2024/node/s34_dynamicHTML/index.js
@@ -1,9 +1,27 @@
 ﻿const express = require('express');
 const app = express();
-const port = 8080;
+const port = 3000;
+const path = require('path');
+
+app.set('view engine', 'ejs');
+// __dirname 혹은 소스가 있는 디렉토리
+app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
-    res.send("HI!")
+    //res.send(`HI! ${__dirname}`)
+    //const num = Math.floor(Math.random() * 10) + 1;
+    res.render('home');
+});
+
+app.get('/rand', (req, res) => {
+    //res.send(`HI! ${__dirname}`)
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random', { rand: num });
+});
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.render('subreddit', {subreddit});
 });
 
 app.listen(port, () => {
