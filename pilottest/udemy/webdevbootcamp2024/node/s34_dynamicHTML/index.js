b9d0333b137ae3f385ad0c6b50b0d837b83b7186
What5up7 <bumsup.kim@gmail.com>
2024-05-21 08:56:25 +0900

    20240521.개인pc

--- pilottest/udemy/webdevbootcamp2024/node/s34_dynamicHTML/index.js
+++ pilottest/udemy/webdevbootcamp2024/node/s34_dynamicHTML/index.js
@@ -2,9 +2,14 @@
 const app = express();
 const port = 3000;
 const path = require('path');
+const redditData = require('./data.json');
 
 app.set('view engine', 'ejs');
-// __dirname 혹은 소스가 있는 디렉토리
+/* 
+    __dirname 혹은 소스가 있는 디렉토리
+    path.join을 쓰는 이유는 node, nodemon을 상위 경로에서 실행할 수 있기 때문
+    예) node /s34_dynamicHTML/index.js    
+*/
 app.set('views', path.join(__dirname, '/views'));
 
 app.get('/cats', (req, res) => {
@@ -26,13 +31,25 @@ app.get('/', (req, res) => {
 app.get('/rand', (req, res) => {
     //res.send(`HI! ${__dirname}`)
     const num = Math.floor(Math.random() * 10) + 1;
-    res.render('random', { rand: num });
+    res.render('random', { rand: num, title: 'random', name: 'random' });
 });
 
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
-    res.render('subreddit', {subreddit});
+    const data = redditData[subreddit];
+    if (data) {
+        res.render('subreddit', { ...data});
+    }
+    else
+    {
+        res.render('notfound', { subreddit});
+    }
+    
+    //res.render('subreddit', {subreddit});
 });
+// 그것
+app.use(express.static(path.join(__dirname, 'static')));
+
 
 app.listen(port, () => {
     console.log(`LISTENING ON PORT ${port}`);
